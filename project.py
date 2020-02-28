49c2a5ac33769c50b6ca9526c69a93933dc08f9a
jhe19 <36580192+jhe19@users.noreply.github.com>
2020-02-28 17:41:10 -0500

    included new query for getting all masters/grad student married couples

--- project.py
+++ project.py
@@ -9,8 +9,88 @@ df.columns = df.columns.str.strip()
 
 con = sqlite3.connect("census_stuff.db")
 
+c = con.cursor()
+c.execute('DROP TABLE IF EXISTS "people";')
 # drop data into database
 df.to_sql("people", con)
+
+
+command1 = '''
+SELECT
+  *
+FROM
+  people
+;
+'''
+
+# c.execute(command1)
+# for r in c:
+#     print(r)
+
+# command2 = '''
+# SELECT *
+# FROM people
+# WHERE (EDUC_SP IS NULL)
+# OR (FAMUNIT_SP IS NULL)
+# OR (SPLOC_SP IS NULL)
+# OR (SPRULE_SP IS NULL)
+# OR (BIRTHYR_SP IS NULL)
+# OR (RACE_SP IS NULL)
+# OR (RACED_SP IS NULL)
+# OR (BPL_SP IS NULL)
+# OR (BPLD_SP IS NULL)
+# OR (EDUC_SP IS NULL)
+# OR (EDUCD_SP IS NULL)
+# OR (OCC_SP IS NULL)
+# OR (IND_SP IS NULL)
+# OR (INCTOT_SP IS NULL)
+# OR (FTOTINC_SP IS NULL)
+# OR (INCWAGE_SP IS NULL)
+#
+# '''
+# c.execute(command2)
+# for r in c:
+#     print(r)
+
+#delete rows where spouse is none
+command3_delete_spouse = '''
+DELETE
+FROM people
+WHERE (EDUC_SP IS NULL)
+OR (FAMUNIT_SP IS NULL)
+OR (SPLOC_SP IS NULL)
+OR (SPRULE_SP IS NULL)
+OR (BIRTHYR_SP IS NULL)
+OR (RACE_SP IS NULL)
+OR (RACED_SP IS NULL)
+OR (BPL_SP IS NULL)
+OR (BPLD_SP IS NULL)
+OR (EDUC_SP IS NULL)
+OR (EDUCD_SP IS NULL)
+OR (OCC_SP IS NULL)
+OR (IND_SP IS NULL)
+OR (INCTOT_SP IS NULL)
+OR (FTOTINC_SP IS NULL)
+OR (INCWAGE_SP IS NULL)
+
+'''
+c.execute(command3_delete_spouse)
+
+#print out database rows
+# c.execute(command1)
+# for r in c:
+#     print(r)
+
+command_onlyGrad = '''
+SELECT EDUCD, EDUCD_SP
+FROM people
+WHERE (EDUCD_SP = 114 OR  EDUCD_SP = 115 OR EDUCD_SP = 115)
+AND (EDUCD = 114 OR  EDUCD = 115 OR EDUCD = 115)
+'''
+#print out database rows of only masters/grad students
+c.execute(command_onlyGrad)
+for r in c:
+    print(r)
 
 
 
