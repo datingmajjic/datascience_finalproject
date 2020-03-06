6f21d440e47e04c4ab37f200c7ba5328be1ce9b3
agoodcupofjoe <57635284+agoodcupofjoe@users.noreply.github.com>
2020-03-05 22:41:20 -0500

    created [iid, field_cd] reference

--- kaggle_cleaning.py
+++ kaggle_cleaning.py
@@ -1,11 +1,27 @@
 import sqlite3
 import chardet
 import pandas as pd
+import csv
 
-# Load kaggle data
-with open('Speed_Dating_Data.csv', 'rb') as f:
-    result = chardet.detect(f.read())
+# Load Kaggle data
+kaggle_data = []
 
-kaggle_data = pd.read_csv('Speed_Dating_Data.csv', encoding=result['encoding'])
+with open('Speed_Dating_Data.csv', 'r', encoding='mac_roman', newline='') as csvDataFile:
+    csvReader = csv.reader(csvDataFile, delimiter = ',')
+    for row in csvReader:
+        kaggle_data.append(row)
 
-print(kaggle_data)
+tracker_list = []
+fos_list = [['iid', 'field_cd']]
+for index in range(1, len(kaggle_data)):
+    iid = kaggle_data[index][0]
+    if iid not in tracker_list:
+        # Column position of field_cd is: 35
+        fos_list.append([int(iid), int(kaggle_data[index][35])])
+        tracker_list.append(iid)
+
+for index in range(1, 18):
+    new_column = str(index) + '_'
+
+for entry in fos_list:
+    print(entry)
