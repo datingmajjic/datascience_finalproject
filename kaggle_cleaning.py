0e16d94ff563a4bcbc13ee5f384bb008704bbc9a
agoodcupofjoe <57635284+agoodcupofjoe@users.noreply.github.com>
2020-03-07 16:10:30 -0500

    career, age, race retrieved

--- kaggle_cleaning.py
+++ kaggle_cleaning.py
@@ -12,16 +12,49 @@ with open('Speed_Dating_Data.csv', 'r', encoding='mac_roman', newline='') as csv
         kaggle_data.append(row)
 
 tracker_list = []
-fos_list = [['iid', 'field_cd']]
+
+# Field of study and career list
+occupation_list = [['iid', 'field_cd', 'career_c']]
 for index in range(1, len(kaggle_data)):
     iid = kaggle_data[index][0]
     if iid not in tracker_list:
         # Column position of field_cd is: 35
-        fos_list.append([int(iid), int(kaggle_data[index][35])])
+        # Column position of career_c is: 49
+        if kaggle_data[index][35] != '' and kaggle_data[index][49] != '':
+            occupation_list.append([int(iid), int(kaggle_data[index][35]), int(kaggle_data[index][49])])
+        else:
+            if kaggle_data[index][35] == '' and kaggle_data[index][49] == '':
+                occupation_list.append([int(iid), None, None])
+            elif kaggle_data[index][35] == '':
+                occupation_list.append([int(iid), None, int(kaggle_data[index][49])])
+            elif kaggle_data[index][49] == '':
+                occupation_list.append([int(iid), int(kaggle_data[index][35]), None])
         tracker_list.append(iid)
 
-for index in range(1, 18):
-    new_column = str(index) + '_'
+tracker_list = []
+
+# Age and race list
+demographics_list = [['iid', 'age', 'race']]
+for index in range(1, len(kaggle_data)):
+    iid = kaggle_data[index][0]
+    if iid not in tracker_list:
+        # Column position of age is: 33
+        # Column position of race is: 39
+        if kaggle_data[index][33] != '' and kaggle_data[index][39] != '':
+            demographics_list.append([int(iid), int(kaggle_data[index][33]), int(kaggle_data[index][39])])
+        else:
+            if kaggle_data[index][33] == '' and kaggle_data[index][39] == '':
+                demographics_list.append([int(iid), None, None])
+            elif kaggle_data[index][33] == '':
+                demographics_list.append([int(iid), None, int(kaggle_data[index][39])])
+            elif kaggle_data[index][39] == '':
+                demographics_list.append([int(iid), int(kaggle_data[index][35]), None])
+        tracker_list.append(iid)
+
+
+
+# for index in range(1, 18):
+#     new_column = str(index) + '_'
 
 for entry in fos_list:
     print(entry)
