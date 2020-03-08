490e123c01ec4e49c3e800b464c9cb8f8e0a6ad3
agoodcupofjoe <57635284+agoodcupofjoe@users.noreply.github.com>
2020-03-08 16:38:14 -0400

    database created

--- kaggle_cleaning.py
+++ kaggle_cleaning.py
@@ -75,12 +75,12 @@ for index in range(1, 19):
 fos_date_results = [['iid'] + fos_date_results]
 for index in range(num_iids + 1):
     new_iid = [index + 1]
-    scores = [0 for x in fos_date_results[0][1:]]
+    scores = [None for x in fos_date_results[0][1:]]
     new_entry = new_iid + scores
     fos_date_results.append(new_entry)
 
 previous_iid = 0
-total_tracker = [0 for x in range(len(fos_date_results[0]))]
+total_tracker = [None for x in range(len(fos_date_results[0]))]
 
 for entry in kaggle_data[1:]:
     # Column position of iid is: 0
@@ -89,10 +89,10 @@ for entry in kaggle_data[1:]:
     if current_iid != previous_iid and previous_iid != 0:
         new = [previous_iid]
         for (num, den) in zip(fos_date_results[previous_iid][1:], total_tracker[1:]):
-            if den != 0:
+            if den != None and num != None:
                 new.append(num / den)
             else:
-                new.append(0)
+                new.append(None)
         fos_date_results[previous_iid] = new
         total_tracker = [0 for x in range(len(fos_date_results[0]))]
 
@@ -119,9 +119,19 @@ for entry in kaggle_data[1:]:
     for index in entry_indices:
         if index != 106:
             if entry[index] != '':
+                if fos_date_results[current_iid][fdr_start_index] == None:
+                    fos_date_results[current_iid][fdr_start_index] = 0
+                if total_tracker[fdr_start_index] == None:
+                    total_tracker[fdr_start_index] = 0
+
                 fos_date_results[current_iid][fdr_start_index] += float(entry[index])
                 total_tracker[fdr_start_index] += 1
         else:
+            if fos_date_results[current_iid][fdr_start_index] == None:
+                fos_date_results[current_iid][fdr_start_index] = 0
+            if total_tracker[fdr_start_index] == None:
+                total_tracker[fdr_start_index] = 0
+
             fos_date_results[current_iid][fdr_start_index] += 1
             total_tracker[fdr_start_index] = 1
 
@@ -131,12 +141,11 @@ for entry in kaggle_data[1:]:
 
 new = [previous_iid]
 for (num, den) in zip(fos_date_results[previous_iid][1:], total_tracker[1:]):
-    if den != 0:
+    if den != None and num != None:
         new.append(num / den)
     else:
-        new.append(0)
+        new.append(None)
 fos_date_results[previous_iid] = new
-
 # fos_date_results is where data for results based on field of study is stored
 ###############################################################################
 
@@ -152,12 +161,12 @@ for index in range(1, 18):
 career_date_results = [['iid'] + career_date_results]
 for index in range(num_iids + 1):
     new_iid = [index + 1]
-    scores = [0 for x in career_date_results[0][1:]]
+    scores = [None for x in career_date_results[0][1:]]
     new_entry = new_iid + scores
     career_date_results.append(new_entry)
 
 previous_iid = 0
-total_tracker = [0 for x in range(len(career_date_results[0]))]
+total_tracker = [None for x in range(len(career_date_results[0]))]
 
 for entry in kaggle_data[1:]:
     # Column position of iid is: 0
@@ -166,12 +175,12 @@ for entry in kaggle_data[1:]:
     if current_iid != previous_iid and previous_iid != 0:
         new = [previous_iid]
         for (num, den) in zip(career_date_results[previous_iid][1:], total_tracker[1:]):
-            if den != 0:
+            if den != None and num != None:
                 new.append(num / den)
             else:
-                new.append(0)
+                new.append(None)
         career_date_results[previous_iid] = new
-        total_tracker = [0 for x in range(len(career_date_results[0]))]
+        total_tracker = [None for x in range(len(career_date_results[0]))]
 
     # Column position of pid is: 11
     if entry[11] == '':
@@ -196,9 +205,19 @@ for entry in kaggle_data[1:]:
     for index in entry_indices:
         if index != 106:
             if entry[index] != '':
+                if career_date_results[current_iid][career_start_index] == None:
+                    career_date_results[current_iid][career_start_index] = 0
+                if total_tracker[career_start_index] == None:
+                    total_tracker[career_start_index] = 0
+
                 career_date_results[current_iid][career_start_index] += float(entry[index])
                 total_tracker[career_start_index] += 1
         else:
+            if career_date_results[current_iid][career_start_index] == None:
+                career_date_results[current_iid][career_start_index] = 0
+            if total_tracker[career_start_index] == None:
+                total_tracker[career_start_index] = 0
+
             career_date_results[current_iid][career_start_index] += 1
             total_tracker[career_start_index] = 1
 
@@ -208,92 +227,166 @@ for entry in kaggle_data[1:]:
 
 new = [previous_iid]
 for (num, den) in zip(career_date_results[previous_iid][1:], total_tracker[1:]):
-    if den != 0:
+    if den != None and num != None:
         new.append(num / den)
     else:
-        new.append(0)
+        new.append(None)
 career_date_results[previous_iid] = new
 
-for entry in career_date_results:
-    print(entry)
+# for entry in career_date_results:
+#     print(entry)
 # career_date_results is where data for results based on intended career is stored
 ###############################################################################
 
+# ###############################################################################
+# # Create a database for how each iid rated a certain race
+# results = ['decision', 'attractive', 'sincere', 'intelligent', 'fun', 'ambitious', 'share', 'like', 'probability', 'total']
+# race_date_results = []
+# for index in range(1, 7):
+#     for result in results:
+#         new_column = str(index) + '_' + result
+#         race_date_results.append(new_column)
+
+# race_date_results = [['iid'] + race_date_results]
+# for index in range(num_iids + 1):
+#     new_iid = [index + 1]
+#     scores = [0 for x in race_date_results[0][1:]]
+#     new_entry = new_iid + scores
+#     race_date_results.append(new_entry)
+
+# previous_iid = 0
+# total_tracker = [0 for x in range(len(race_date_results[0]))]
+
+# for entry in kaggle_data[1:]:
+#     # Column position of iid is: 0
+#     current_iid = int(entry[0])
+
+#     if current_iid != previous_iid and previous_iid != 0:
+#         new = [previous_iid]
+#         for (num, den) in zip(race_date_results[previous_iid][1:], total_tracker[1:]):
+#             if den != 0:
+#                 new.append(num / den)
+#             else:
+#                 new.append(0)
+#         race_date_results[previous_iid] = new
+#         total_tracker = [0 for x in range(len(race_date_results[0]))]
+
+#     # Column position of pid is: 11
+#     if entry[11] == '':
+#         previous_iid = current_iid
+#         continue
+#     else:
+#         partner_iid = int(entry[11])
+
+#     partner_race = participant_info[partner_iid][4]
+#     if partner_race == None:
+#         previous_iid = current_iid
+#         continue
+
+#     # Indices for where to find findings in entry:
+#     # Column position of dec/decision is: 97
+#     # Column position of prob/probability is: 105
+#     entry_indices = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+
+#     # Index for where to start logging findings in race_date_results:
+#     race_start_index = ((partner_race - 1) * 10) + 1
+
+#     for index in entry_indices:
+#         if index != 106:
+#             if entry[index] != '':
+#                 race_date_results[current_iid][race_start_index] += float(entry[index])
+#                 total_tracker[race_start_index] += 1
+#         else:
+#             race_date_results[current_iid][race_start_index] += 1
+#             total_tracker[race_start_index] = 1
+
+#         race_start_index += 1
+
+#     previous_iid = current_iid
+
+# new = [previous_iid]
+# for (num, den) in zip(race_date_results[previous_iid][1:], total_tracker[1:]):
+#     if den != 0:
+#         new.append(num / den)
+#     else:
+#         new.append(0)
+# race_date_results[previous_iid] = new
+
+# for entry in race_date_results:
+#     print(entry)
+# # race_date_results is where data for results based on race is stored
+# ###############################################################################
+
 ###############################################################################
-# Create a database for how each iid rated a certain race
-results = ['decision', 'attractive', 'sincere', 'intelligent', 'fun', 'ambitious', 'share', 'like', 'probability', 'total']
-race_date_results = []
-for index in range(1, 7):
-    for result in results:
-        new_column = str(index) + '_' + result
-        race_date_results.append(new_column)
+# Insert array into sql
 
-race_date_results = [['iid'] + race_date_results]
-for index in range(num_iids + 1):
-    new_iid = [index + 1]
-    scores = [0 for x in race_date_results[0][1:]]
-    new_entry = new_iid + scores
-    race_date_results.append(new_entry)
+# Create connection to database
+conn = sqlite3.connect('kaggle_clean.db')
+c = conn.cursor()
 
-previous_iid = 0
-total_tracker = [0 for x in range(len(race_date_results[0]))]
+# Added by student (because of sql lab):
+c.execute('PRAGMA foreign_keys = ON')
 
-for entry in kaggle_data[1:]:
-    # Column position of iid is: 0
-    current_iid = int(entry[0])
+# Delete tables if they exist
+c.execute('DROP TABLE IF EXISTS "field_of_study";')
+c.execute('DROP TABLE IF EXISTS "career";')
 
-    if current_iid != previous_iid and previous_iid != 0:
-        new = [previous_iid]
-        for (num, den) in zip(race_date_results[previous_iid][1:], total_tracker[1:]):
-            if den != 0:
-                new.append(num / den)
-            else:
-                new.append(0)
-        race_date_results[previous_iid] = new
-        total_tracker = [0 for x in range(len(race_date_results[0]))]
+# Commands to create the 'field_of_study' and 'career' tables
+commands = ['decision', 'attractive', 'sincere', 'intelligent', 'fun', 'ambitious', 'share', 'like', 'probability', 'total']
+create_field_of_study = '''CREATE TABLE field_of_study (iid INT PRIMARY KEY NOT NULL,'''
+create_career = '''CREATE TABLE career (iid INT PRIMARY KEY NOT NULL,'''
 
-    # Column position of pid is: 11
-    if entry[11] == '':
-        previous_iid = current_iid
-        continue
-    else:
-        partner_iid = int(entry[11])
+for index in range(1, 19):
+    for entry in commands:
+        create_field_of_study = create_field_of_study + ' "' + str(index) + '_' + entry + '" FLOAT,'
+create_field_of_study = create_field_of_study[:-1] + ');'
 
-    partner_race = participant_info[partner_iid][4]
-    if partner_race == None:
-        previous_iid = current_iid
-        continue
+for index in range(1, 18):
+    for entry in commands:
+        create_career = create_career + ' "' + str(index) + '_' + entry + '" FLOAT,'
+create_career = create_career[:-1] + ');'
 
-    # Indices for where to find findings in entry:
-    # Column position of dec/decision is: 97
-    # Column position of prob/probability is: 105
-    entry_indices = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+# Execute the create tables commands
+c.execute(create_field_of_study)
+conn.commit()
 
-    # Index for where to start logging findings in race_date_results:
-    race_start_index = ((partner_race - 1) * 10) + 1
+c.execute(create_career)
+conn.commit()
 
-    for index in entry_indices:
-        if index != 106:
-            if entry[index] != '':
-                race_date_results[current_iid][race_start_index] += float(entry[index])
-                total_tracker[race_start_index] += 1
+# Insert values into tables
+for entry in fos_date_results[1:]:
+    insert_fos = 'INSERT INTO field_of_study VALUES ('
+    for value in entry:
+        if value == None:
+            insert_fos += 'NULL'
         else:
-            race_date_results[current_iid][race_start_index] += 1
-            total_tracker[race_start_index] = 1
+            insert_fos += str(value)
+        insert_fos += ', '
+    insert_fos = insert_fos[:-2] + ');'
 
-        race_start_index += 1
+    # Execute and commit the commands
+    c.execute(insert_fos)
+    conn.commit()
 
-    previous_iid = current_iid
+for entry in career_date_results[1:]:
+    insert_career = 'INSERT INTO career VALUES ('
+    for value in entry:
+        if value == None:
+            insert_career += 'NULL'
+        else:
+            insert_career += str(value)
+        insert_career += ', '
+    insert_career = insert_career[:-2] + ');'
 
-new = [previous_iid]
-for (num, den) in zip(race_date_results[previous_iid][1:], total_tracker[1:]):
-    if den != 0:
-        new.append(num / den)
-    else:
-        new.append(0)
-race_date_results[previous_iid] = new
+    # Execute and commit the commands
+    c.execute(insert_career)
+    conn.commit()
 
-for entry in race_date_results:
-    print(entry)
-# race_date_results is where data for results based on race is stored
-###############################################################################
+
+
+
+
+
+
+
+
