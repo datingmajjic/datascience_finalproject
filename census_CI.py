a19cb708ade14a18e047d766bb1408bf807bfbe2
Milla Shin <mshin7@cs.brown.edu>
2020-04-08 04:31:53 -0400

    added z-tests for census data

--- census_CI.py
+++ census_CI.py
@@ -1,8 +1,10 @@
 import sqlite3
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import chisquare
+import census_Chi_squared as census_analysis
+from statsmodels.stats import proportion
 
 #counts the number of ppl with that career in both datasets
 def countField(dfC, dfK, field):
@@ -79,3 +81,121 @@ if __name__=='__main__':
 
 
     # print(countO)
+
+
+    ###########################################################################
+    # Analyzing CI
+    ###########################################################################
+    dfKCareer = load_file("kaggle_clean.db", "SELECT * FROM career")
+    dfKField = load_file("kaggle_clean.db", "SELECT * FROM field_of_study")
+
+    ###########################################################################
+    # Analyzing CI for occupations for Kaggle dataset
+    ###########################################################################
+    sameOccupationKaggle = census_analysis.sameOCCKaggle(dfK, dfKCareer)
+    objects = ('Num people who said yes more times to go on another date with \
+        someone of the same occupation',
+        'Num people who said no more times to go on another date with \
+            someone of the same occupation')
+    plt.figure(1)
+    y_pos = np.arange(len(objects))
+    total_sameOcc = np.sum(sameOccupationKaggle, axis=1)[0]
+    total_diffOcc = np.sum(sameOccupationKaggle, axis=1)[1]
+    values = [total_sameOcc, total_diffOcc]
+    plt.bar(y_pos, values, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Total number of people')
+    plt.title('Attraction based on occupation in Kaggle dataset')
+    # plt.show()
+
+    ###########################################################################
+    # Analyzing CI for majors for Kaggle dataset
+    ###########################################################################
+    sameFKaggle = census_analysis.sameFieldKaggle(dfK, dfKField)
+    plt.figure(3)
+
+
+    ###########################################################################
+    # Analyzing CI for occupations for census dataset
+    ###########################################################################
+    sameOccupationCensus = census_analysis.sameOCCCensus(dfC)
+    objects = ('married to someone with same occupation',
+        'married to someone with different occupation')
+    plt.figure(2)
+    y_pos = np.arange(len(objects))
+    total_sameOcc = np.sum(sameOccupationCensus, axis=1)[0]
+    percent_marriedSameOcc = total_sameOcc/len(dfC)
+    total_diffOcc = np.sum(sameOccupationCensus, axis=1)[1]
+    percent_marriedDiffOcc = total_diffOcc/len(dfC)
+    values = [total_sameOcc, total_diffOcc]
+    plt.bar(y_pos, values, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Total number of people')
+    plt.title('Marriage based on occupation in US census dataset')
+    # plt.show()
+
+    # hypothesis is that "People tend to marry others in the same occupation"
+    # ie. ">50% of people marry someone in same occupation"
+    # H0 = P >= 50%
+    # Ha = P < 50%
+    print("Z-test for census data occupation")
+    print("Percentage of married couples with same occ = %f" % (percent_marriedSameOcc))
+    stat, pval = proportion.proportions_ztest(total_sameOcc, len(dfC), 0.5, 'smaller')
+    print("test statistic = %f, pval = %f" % (stat, pval))
+    # pval = 0.000 which is less than 0.05, so we reject null hypothesis
+
+
+    ###########################################################################
+    # Analyzing CI for majors for census dataset
+    ###########################################################################
+    sameFCensus = census_analysis.sameFieldCensus(dfC)
+    plt.figure(4)
+    objects = ('married to someone with same major',
+        'married to someone with different major')
+    y_pos = np.arange(len(objects))
+    total_sameField = np.sum(sameFCensus, axis=1)[0]
+    percent_marriedSameF = total_sameField/len(dfC)
+    total_diffField = np.sum(sameFCensus, axis=1)[1]
+    percent_marriedDiffF = total_diffField/len(dfC)
+    values = [total_sameField, total_diffField]
+    plt.bar(y_pos, values, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Total number of people')
+    plt.title('Marriage based on major in US census dataset')
+    # plt.show()
+    # hypothesis is that "People tend to marry others in the same major"
+    # ie. ">50% of people marry someone in same major"
+    # H0 = P >= 50%
+    # Ha = P < 50%
+    print("Z-test for census data major")
+    stat, pval = proportion.proportions_ztest(total_sameField, len(dfC), 0.5, 'smaller')
+    print("Percentage of married couples with same major = %f" % (percent_marriedSameF))
+    print("test statistic = %f, pval = %f" % (stat, pval))
+    # pval = 0.000 which is less than 0.05, so we reject null hypothesis
+
+    ###########################################################################
+    # Analyzing occ vs. major for census data set
+    ###########################################################################
+    # hypothesis is that "Having the occupation is a better predictor of marriage
+    # than having the same college major"
+    # ie. "the proportion of married people with the same occupation  (p_o) is greater
+    # than the proportion of married people with the same college major (p_m)"
+    # H0 = proportions same
+    # Ha = proportions diff (p_o > p_m)))
+    plt.figure(5)
+    objects = ('married to someone with same occupation',
+        'married to someone with same major')
+    y_pos = np.arange(len(objects))
+    values = [percent_marriedSameOcc, percent_marriedSameF]
+    plt.bar(y_pos, values, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Percentage out of all married couples')
+    plt.title('Marriage based on major and occupation in US census dataset')
+
+    print("Z-test for census data major vs. occupation")
+    count = np.array([total_sameOcc, total_sameField])
+    nobs = np.array([ len(dfC),  len(dfC)])
+    stat, pval = proportion.proportions_ztest(count,nobs, 0, 'larger')
+    print("test statistic = %f, pval = %f" % (stat, pval))
+    # pval = 0.000 which is less than 0.05, so we reject null hypothesis
+    plt.show()
